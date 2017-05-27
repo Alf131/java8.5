6c877e61f53c21754f944faf56914f7a1bd90f81
Alf Chlopecki <chlopecki13@gmail.com>
2017-05-27 12:10:45 +0200

    changed alert text

--- js/index.js
+++ js/index.js
@@ -4,5 +4,5 @@ var a = prompt('Enter triangle base a');
 var h = prompt('Enter triangle height h');
 var triangleArea = 1.10
 triangleArea = a * h/2
-alert('Pole trójkąta wynosi, ' + triangleArea);
+alert('Triangle field is, ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
