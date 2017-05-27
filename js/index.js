16bf8ca60fea83854f79e036be6f75960e3e0ecb
Alf Chlopecki <chlopecki13@gmail.com>
2017-05-27 12:08:16 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = prompt('Enter triangle base a');
+var h = prompt('Enter triangle height h');
+var triangleArea = 1.10
+triangleArea = a * h/2
+alert('Pole trójkąta wynosi, ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
