c3a0a8b523befb09bdd92500eb245201156b17e0
Alf Chlopecki <chlopecki13@gmail.com>
2017-05-27 15:17:42 +0200

    removed triangleArea variable

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 
 var a = prompt('Enter triangle base a');
 var h = prompt('Enter triangle height h');
-var triangleArea = 1.10
-triangleArea = a * h/2
+triangleArea = a * h/2;
 alert('Triangle field is, ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
